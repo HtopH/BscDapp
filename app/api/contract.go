e00a596842415e4e78dac92a1a42c3aace673769
top <top@top>
2021-11-26 17:47:17 +0800

    from company

--- app/api/contract.go
+++ app/api/contract.go
@@ -4,10 +4,34 @@ var ContractApi = contractApi{}
 
 type contractApi struct{}
 
-// @summary PC网页支付
+// @summary 注册
 // @tags   合约接口
 // @produce json
 // @param   ref_addr formData string true "推荐人地址,无则传空字符串 "
-// @param   orderNo formData string true "订单号"
-// @router  register [POST]
-// @success 200 {object} model.MpRes "执行结果"
+// @router  /register [POST]
+// @success 200 {object} service.JsonResponse "执行结果"
+func (a *contractApi) Register() {}
+
+// @summary 购买门票
+// @tags   合约接口
+// @produce json
+// @param   _value formData int true "支付金额,big.int,需乘精度"
+// @router  /payForTickets [POST]
+// @success 200 {object} service.JsonResponse "执行结果"
+func (a *contractApi) PayForTickets() {}
+
+// @summary 投资
+// @tags   合约接口
+// @produce json
+// @param   _value formData int true "支付金额,big.int,需乘精度"
+// @router  /joinGame [POST]
+// @success 200 {object} service.JsonResponse "执行结果"
+func (a *contractApi) JoinGame() {}
+
+// @summary 会员提现
+// @tags   合约接口
+// @produce json
+// @param   _value formData int true "提现金额,big.int,需乘精度,不需用户填,直接传会员余额"
+// @router  /userWithdraw [POST]
+// @success 200 {object} service.JsonResponse "执行结果"
+func (a *contractApi) UserWithdraw() {}
